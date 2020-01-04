574ee45f409ec1fd6d5f3a699ecccd6e78410af5
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-04 17:35:58 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+ result = [] 
+ result = array.each_with_index {|index, item| 
+                          puts "/#{item+1}.*#{index}/" }
 end
 
 def summon_captain_planet# code an argument here
